d687c700b42be26c33162347f3ded2c090e55fca
Liel <liel.izcha321@gmail.com>
2021-08-27 13:11:46 +0300

    1-6

---
+++ main.js
@@ -0,0 +1,20 @@
+//? exeresie 1
+console.log("israel");
+//? exeresie 2
+console.log("liel");
+//? exeresie 3
+console.log(4);
+//? exeresie 4
+console.log(2, 5, 1, 8, 6);
+// ? exeresie 5
+var num1 = 1,
+  num2 = 2,
+  num3 = 3,
+  num4 = 4;
+var avg = num1 + num2 + num3 + num4 / 4;
+console.log(avg);
+// ? exeresie 6
+var width = 15,
+  hight = 2,
+  sum = width * hight;
+  console.log(sum);
